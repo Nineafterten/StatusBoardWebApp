c02f4f2122b257529fb689459b700a58b1d38913
Doug Greene <doug@l4mobile.com>
2015-07-22 10:17:12 -0700

    Removed some components, got gulp to build a local server and watch for less changes with live reload.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,28 @@
-var gulp = require("gulp");
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+var less = require('gulp-less');
 
-gulp.task("default", function () {
-    console.log("Default task is to build. See README.md for more info.");
+gulp.task('connect', function() {
+  connect.server({
+    livereload: true
+  });
 });
 
+gulp.task('html', function () {
+  gulp.src('./src/app/*.html')
+    .pipe(connect.reload());
+});
+
+gulp.task('watch', function () {
+  gulp.watch(['./src/app/*.html'], ['html']);
+  gulp.watch(['./src/styles/*.less'], ['less']);
+});
+
+gulp.task('less', function () {
+  gulp.src('./src/styles/index.less')
+  .pipe(less())
+  .pipe(gulp.dest('./src/styles/'))
+  .pipe(connect.reload());
+});
+
+gulp.task('default', ['connect', 'watch']);
