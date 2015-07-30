6b93bc05c89bf9bc0445ebbd65f468d949da8e75
Doug Greene <doug@l4mobile.com>
2015-07-29 23:23:29 -0700

    Updated gulp tasks handle Less Compiler. All Less files are now being watched. Set up variables.less as single import dependency for styling. Gulp error notify and plumber running. Fancy stuff.

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,51 @@
-var gulp = require('gulp');
-var connect = require('gulp-connect');
-var less = require('gulp-less');
+var gulp =          require("gulp");
+var concat =        require("gulp-concat");
+var connect =       require("gulp-connect");
+var less =          require("gulp-less");
+var notify =        require("gulp-notify");
+var plumber =       require("gulp-plumber");
+var sourcemaps =    require("gulp-sourcemaps");
 
-gulp.task('connect', function() {
-  connect.server({
-    livereload: true
-  });
+gulp.task("connect", function() {
+    connect.server({
+        livereload: true
+    });
 });
 
-gulp.task('html', function () {
-  gulp.src('./src/app/*.html')
+gulp.task("watch", function () {
+    gulp.watch(["./src/app/*.html"], ["html"]);
+    gulp.watch([
+        "./src/app/**/*.less",
+        "./src/styles/*.less",
+        "./src/styles/**/*.less"
+    ], ["less-compiler"]);
+});
+
+gulp.task("html", function () {
+    gulp.src("./src/app/*.html")
+        .pipe(connect.reload());
+});
+
+gulp.task("less-compiler", function () {
+    gulp.src([
+        "./src/app/**/*.less",
+        "./src/styles/*.less",
+        "./src/styles/**/*.less"
+    ])
+    .pipe(plumber({
+        errorHandler: notify.onError("Less Compiler: <%= error.message %>")
+    }))
+    .pipe(sourcemaps.init())
+    .pipe(less([
+            "./src/styles/",
+            "../bower_components/bootstrap/less",
+            "../bower_components/font-awesome/less"
+        ]
+    ))
+    .pipe(concat("index.css"))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest("./src/styles/"))
     .pipe(connect.reload());
 });
 
-gulp.task('watch', function () {
-  gulp.watch(['./src/app/*.html'], ['html']);
-  gulp.watch(['./src/styles/*.less'], ['less']);
-});
-
-gulp.task('less', function () {
-  gulp.src('./src/styles/index.less')
-  .pipe(less())
-  .pipe(gulp.dest('./src/styles/'))
-  .pipe(connect.reload());
-});
-
-gulp.task('default', ['connect', 'watch']);
+gulp.task("default", ["connect", "watch"]);
