eb216d26418656a81256a0800ec2bcfdd9e370d2
Doug Greene <doug@l4mobile.com>
2015-08-10 22:20:38 -0700

    Google maps loaded with traffic overlay for Seattle (need to do custom styling of maps next).

--- src/app/components/traffic/traffic.module.js
+++ src/app/components/traffic/traffic.module.js
@@ -1,7 +1,21 @@
 angular.module('traffic', [])
 
 .controller('trafficController', ['$scope', function($scope) {
-    console.log('trafficController loaded');
+
+    function initialize() {
+        var myLatlng = new google.maps.LatLng(47.614848,-122.3359058);
+        var mapOptions = {
+        zoom: 12,
+        center: myLatlng
+    };
+
+    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+    var trafficLayer = new google.maps.TrafficLayer();
+        trafficLayer.setMap(map);
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize);
 
 }])
 
