d206af3cdd4253493f52868150a6b25cd73677ac
Doug Greene <doug@l4mobile.com>
2015-08-10 23:06:00 -0700

    First stab at custom styling google maps (crazy stuff, will need to take finer-grained attempt at it again)

--- src/app/components/traffic/traffic.module.js
+++ src/app/components/traffic/traffic.module.js
@@ -3,17 +3,91 @@ angular.module('traffic', [])
 .controller('trafficController', ['$scope', function($scope) {
 
     function initialize() {
-        var myLatlng = new google.maps.LatLng(47.614848,-122.3359058);
-        var mapOptions = {
-        zoom: 12,
-        center: myLatlng
-    };
 
-    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-
+    // load the traffic layer on top
     var trafficLayer = new google.maps.TrafficLayer();
         trafficLayer.setMap(map);
     }
+    // our name for the map toggle style (desktop action)
+    var styledMapOptions = {
+        name: 'L4 Mobile'
+    };
+    // the styling changes to the map
+    var featureOpts = [
+    {
+      stylers: [
+        { color: '#444444' },
+        { visibility: 'simplified' },
+        { gamma: 0.5 },
+        { weight: 0.5 }
+      ]
+    },
+    {
+      featureType: 'road.highway',
+      elementType: 'geometry',
+      stylers: [
+        { color: '#666666' }
+      ]
+    },{
+      featureType: 'road.arterial',
+      elementType: 'all',
+      stylers: [
+        { color: '#cccccc' },
+        { visibility: 'simplified' }
+      ]
+    },{
+      featureType: 'road.local',
+      elementType: 'all',
+      stylers: [
+        { color: '#999999' },
+        { visibility: 'simplified' }
+      ]
+    },{
+      featureType: 'water',
+      elementType: 'geometry',
+      stylers: [
+        { color: '#336699' }
+      ]
+    },{
+      featureType: 'road.highway',
+      elementType: 'labels',
+      stylers: [
+        { visibility: 'off' }
+      ]
+    },{
+      featureType: 'administrative.locality',
+      elementType: 'labels',
+      stylers: [
+        { hue: '#ffffff' }
+      ]
+    },{
+      featureType: 'transit.line',
+      elementType: 'geometry',
+      stylers: [
+        { hue: '#cccccc' },
+        { visibility: 'on' },
+        { lightness: -70 }
+      ]
+    }
+  ];
+
+    var customMapType = new google.maps.StyledMapType(featureOpts, styledMapOptions);
+    // you are here
+    var seattle = new google.maps.LatLng(47.614848,-122.3359058);
+    // map id for customizing
+    var MY_MAPTYPE_ID = 'custom_style';
+    // map options for setting exact location
+    var mapOptions = {
+        zoom: 12,
+        center: seattle,
+        mapTypeControlOptions: {
+            mapTypeIds: [google.maps.MapTypeId.ROADMAP, MY_MAPTYPE_ID]
+        },
+        mapTypeId: MY_MAPTYPE_ID
+    };
+    // get the map element and set it
+    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    map.mapTypes.set(MY_MAPTYPE_ID, customMapType);
 
     google.maps.event.addDomListener(window, 'load', initialize);
 
